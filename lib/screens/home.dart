e9e3b05fc69f0dd35b3870dfeb53fd79d1824957
Stanisław Dera <staszek.dera@gmail.com>
2021-07-13 10:26:19 +0200

    Add divider feature

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:just_note/utils/memory.dart';
+import 'package:just_note/utils/options.dart';
 import 'package:just_note/utils/welcome.dart';
 
 class Home extends StatefulWidget {
@@ -13,6 +14,8 @@ class _HomeState extends State<Home> {
 
   // txt is being used to add note from memory into the TextField
   var txt = TextEditingController();
+
+  final snackBar = SnackBar(content: Text('Yay! A SnackBar!'));
 
   // setup all the stuff
   @override
@@ -33,10 +36,39 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
+
+
+    void _showMoreOptionsPanel() {
+      showModalBottomSheet(context: context, builder: (context) {
+        return Container(
+          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 10.0),
+          child: ListView.builder(
+              itemCount: 1,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: ListTile(
+                    title: Text('Add divider'),
+                    subtitle: Text('Divide one part of your note from another'),
+                    onTap: () => {
+                      Navigator.pop(context),
+                      txt.text += '\n\n=====================\n\n'
+                    },
+                  ),
+                );
+              },
+          )
+        );
+      });
+    }
+
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.pink[400],
         title: Text('JustNote!'),
+        actions: [
+          IconButton(onPressed: () => _showMoreOptionsPanel(), icon: Icon(Icons.more_vert))
+        ],
       ),
       body: Container(
           child: TextField(
---
+++ lib/utils/options.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Options extends StatelessWidget {
+  const Options({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 8.0),
+      child: Card(
+        margin: EdgeInsets.fromLTRB(20.0, 6.0, 20.0, 0.0),
+        child: ListTile(
+          leading: CircleAvatar(
+            radius: 25.0,
+            backgroundColor: Colors.brown[100],
+          ),
+          title: Text('Siema'),
+          subtitle: Text('Takes sugar(s)'),
+        ),
+      ),
+    );
+  }
+}
